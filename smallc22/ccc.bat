2508a4c458a64793ac0d02f84e28b753102f9a78
Zane Wagner <ZaneDubya@users.noreply.github.com>
2016-12-17 11:43:35 -0600

    Move exes to bin

--- smallc22/ccc.bat
+++ smallc22/ccc.bat
@@ -1,28 +1,41 @@
-cc  cc1
-if errorlevel 1 goto exit
-asm cc1 /p
-if errorlevel 1 goto exit
-: del cc1.asm
+ECHO OFF
+REM The -M switch lets us monitor progress by having the compiler write each function header to the screen.
+REM     This also helps isolate errors to the functions containing them.
+REM The -A switch causes the alarm to sound whenever an error is reported.
+REM The -P switch causes the compiler to pause after reporting each error.
+REM     An ENTER (carriage return) keystroke resumes execution.
 
-cc  cc2
+REM CC1
+..\bin\cc  cc1 -a -p
 if errorlevel 1 goto exit
-asm cc2 /p
+..\bin\asm cc1 /p
 if errorlevel 1 goto exit
-: del cc2.asm
 
-cc  cc3
+REM CC2
+..\bin\cc  cc2 -a -p
 if errorlevel 1 goto exit
-asm cc3 /p
+..\bin\asm cc2 /p
 if errorlevel 1 goto exit
-: del cc3.asm
 
-cc  cc4
+REM CC3
+..\bin\cc  cc3 -a -p
 if errorlevel 1 goto exit
-asm cc4 /p
+..\bin\asm cc3 /p
 if errorlevel 1 goto exit
-: del cc4.asm
 
-link cc1 cc2 cc3 cc4,cc,cc,clib.lib
+REM CC4
+..\bin\cc  cc4 -a -p
+if errorlevel 1 goto exit
+..\bin\asm cc4 /p
+if errorlevel 1 goto exit
+
+REM LINK
+..\bin\link cc1 cc2 cc3 cc4,cc,cc,clib.lib
+if errorlevel 1 goto exit
+
+REM CLEANUP
+del *.asm
+del *.obj
 
 :exit
-
+ECHO ON
