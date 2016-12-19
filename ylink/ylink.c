bc8387eaf2d4201471f19a84b537d06a8e016952
Zane Wagner <ZaneDubya@users.noreply.github.com>
2016-12-19 17:24:53 -0600

    Read a file.

--- ylink/ylink.c
+++ ylink/ylink.c
@@ -1,14 +1,17 @@
 // YLINK - the Ypsilon Executable Linker
 // Copyright 2016 Zane Wagner. All rights reserved.
 #define VERSION "Ypsilon Executable Linker, Version 0.01\n"
-#define CRIGHT1 "Copyright 2016 Zane Wagner. All rights reserved.\n\n"
+#define CRIGHT1 "Copyright 2016 Zane Wagner. All rights reserved.\n"
 #include <stdio.h>
 #define LINEMAX  127
 #define LINESIZE 128
+#define ERROR 0
 
-#define ERROR 1
-char errParsingArgs[] = "Error parsing argument:\n";
+char    errParsingArgs[] = "Could not parse argument.",
+        errNoArguments[] = "Requires exactly one argument.";
 char *line;
+
+int     fdObjectFile;
 
 main(int argc, int *argv) {
     fputs(VERSION, stderr);
@@ -17,17 +20,48 @@ main(int argc, int *argv) {
     if (ask(argc, argv) == ERROR) {
         fputs(errParsingArgs, stderr);
     }
-}
-
-ask(int argc, int *argv) {
-    int i;
-    i = 1;
-    while (getarg(i++, line, LINESIZE, argc, argv) != EOF) {
-        fputs(line, stderr);	
+    readFile();
+    if (fdObjectFile != 0) {
+        fclose(fdObjectFile);
     }
-	return;
 }
 
 allocatevars() {
     line = calloc(LINESIZE, 1);
+}
+
+ask(int argc, int *argv) {
+    int i;
+    if (argc != 2) {
+        errout(errNoArguments);
+        return;
+    }
+    getarg(1, line, LINESIZE, argc, argv);
+    if(!(fdObjectFile = fopen(line, "r"))) {
+        errout(line);
+    }
+    i = 1;
+    /*while (getarg(i++, line, LINESIZE, argc, argv) != EOF) {
+        lineout(line);
+        lineout('\n');
+    }*/
+	return 1;
+}
+
+readFile() {
+    char ch;
+    while ((ch = fgetc(fdObjectFile)) != EOF) {
+        putchar(ch);
+    }
+    return;
+}
+
+lineout(char *line, int fd) {
+    fputs(line, fd);
+    fputc(NEWLINE, fd);
+}
+
+errout(char *line) {
+    fputs("*** Error: ", stderr); 
+    lineout(line, stderr);
 }
