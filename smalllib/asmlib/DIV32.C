bad37d37318f61d93eccb0e22b0b1542cab4c4c3
Zane Wagner <ZaneDubya@users.noreply.github.com>
2017-12-19 11:49:09 -0600

    Write new Div32 routine.

--- smalllib/asmlib/DIV32.C
+++ smalllib/asmlib/DIV32.C
@@ -1,126 +1,74 @@
 /*
-** 32 bit x / y -- quotient to x, remainder to y
+** 32 bit n / d -- quotient to n, remainder to d
+** rewrote to get rid of rep cmpsw instruction, which smalla does not handle?
 */
-div32(x, y) unsigned x[], y[]; {
-  int d[4], t[4];
-  d[0] = x[0];
-  d[1] = x[1];
-  #asm
-  MOV   DI,[BP+6]    ; locate x -->  remainder
-  MOV   SI,[BP+4]    ; locate y - divisor
-  LEA   BX,[BP-8]    ; locate d - dividend --> quotient
-;
-; algorithm from Christopher L. Morgan's "Bluebook ..."
-;
-  PUSH DI
-  LEA  DI,[BP-16]      ; locate t - temporary divisor
-  MOV  CX,2
-  CLD
-  REP  MOVSW
-  XOR  AX,AX
-  MOV  CX,2
-  REP  STOSW
-  POP  DI
-  LEA  SI,[BP-16]      ; locate t - temporary divisor
-  MOV  CX,1
-__1:
-  TEST [BP-10],8000H   ; MSB of t
-  JNZ  __2
-  CALL __dsal
-  INC  CX
-  JMP  __1
-__2:
-  CALL __cmp
-  JA   __3
-  CALL __sub
-  STC
-  JMP  __4
-__3:
-  CLC
-__4:
-  CALL __qsal
-  CALL __dslr
-  LOOP __2
-  JMP  __exit
-
-__cmp:
-  PUSH SI
-  PUSH DI
-  PUSH CX
-  STD
-  ADD  SI,6
-  ADD  DI,6
-  MOV  CX,4
-  REP  CMPSW
-  POP  CX
-  POP  DI
-  POP  SI
-  RET
-
-__sub:
-  PUSH SI
-  PUSH DI
-  PUSH CX
-  CLC
-  MOV  CX,4
-__sub1:
-  MOV  AX,[SI]
-  INC  SI
-  INC  SI
-  SBB  [DI],AX
-  INC  DI
-  INC  DI
-  LOOP __sub1
-  POP  CX
-  POP  DI
-  POP  SI
-  RET
-
-__qsal:
-  PUSH BX
-  PUSH CX
-  MOV  CX,2
-__qsal1:
-  RCL  WORD PTR [BX],1
-  INC  BX
-  INC  BX
-  LOOP __qsal1
-  POP  CX
-  POP  BX
-  RET
-
-__dsal:
-  PUSH SI
-  PUSH CX
-  MOV  CX,4
-__dsal1:
-  RCL  WORD PTR [SI],1
-  INC  SI
-  INC  SI
-  LOOP __dsal1
-  POP  CX
-  POP  SI
-  RET 
-
-__dslr:
-  PUSH SI
-  PUSH CX
-  ADD  SI,6
-  MOV  CX,4
-  CLC
-__dslr1:
-  RCR  WORD PTR [SI],1
-  DEC  SI
-  DEC  SI
-  LOOP __dslr1
-  POP  CX
-  POP  SI
-  RET 
-
-__exit:
-  #endasm
-  y[0] = x[0];  y[1] = x[1];
-  x[0] = d[0];  x[1] = d[1];
+div32(unsigned n[], unsigned d[]) {
+  unsigned q[2], r[2];
+  unsigned i, b;
+  if (d[1] == 0 && d[0] == 0) {
+    printf("Error in div32: Divide by zero.\n");
+    abort(1);
   }
+  q[0] = q[1] = 0;
+  r[0] = r[1] = 0;
+  for (i = 31; ; i--) {
+    dv32shl(r);
+    b = 0x0001 << (i % 16);
+    if ((n[i / 16] & b) != 0) {
+      // printf("n[%u] & %x != 0\n", i / 16, b);
+      r[0] |= 0x0001;
+    }
+    if (dv32cmp(r, d) >= 0) {
+      dv32sub(r, d);
+      q[i / 16] |= b;
+    }
+    if (i == 0) {
+      break;
+    }
+  }
+  n[1] = q[1];
+  n[0] = q[0];
+  d[1] = r[1];
+  d[0] = r[0];
+}
 
+// 32 bit x - y to x
+dv32sub(unsigned x[], unsigned y[]) {
+  #asm
+  MOV  BX,[BP+4]       ; locate y low
+  MOV  AX,[BX]         ; fetch y low
+  XCHG CX,BX           ; save address
+  MOV  BX,[BP+6]       ; locate x low
+  SUB  [BX],AX         ; sub y low from x low
+  XCHG CX,BX
+  MOV  AX,[BX+2]       ; fetch y high
+  XCHG CX,BX
+  SBB  [BX+2],AX       ; sub y high from x high with borrow
+  #endasm
+}
 
+// 32 bit compare. returns 1 if a > b, 0 if a == b, -1 if a < b
+dv32cmp(unsigned a[], unsigned b[]) {
+  if (a[1] > b[1]) {
+    return 1;
+  }
+  if (a[1] < b[1]) {
+    return -1;
+  }
+  if (a[0] > b[0]) {
+    return 1;
+  }
+  if (a[0] < b[0]) {
+    return -1;
+  }
+  return 0;
+}
+
+// 32 bit shift a left by 1 bit
+dv32shl(unsigned a[]) {
+  a[1] = a[1] << 1;
+  if (a[0] & 0x8000) {
+    a[1] |= 0x0001;
+  }
+  a[0] = a[0] << 1;
+}
