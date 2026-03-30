6a8e99e788c28d1917334e17aa820f2aafde83c3
Zane Wagner <ZaneDubya@users.noreply.github.com>
2026-03-29 21:18:58 -0500

    Update clean.bat

--- code/smalllib/bld/clean.bat
+++ code/smalllib/bld/clean.bat
@@ -1,5 +1,6 @@
-del *.asm
-del *.obj
-del *.map
-del *.exe
-del *.h
+@ECHO OFF
+if exist *.asm del *.asm
+if exist *.obj del *.obj
+if exist *.map del *.map
+if exist *.exe del *.exe
+if exist *.h del *.h
