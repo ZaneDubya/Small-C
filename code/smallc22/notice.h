307ee68a7b965a290d056d434378ded47cca0fe9
Zane Wagner <ZaneDubya@users.noreply.github.com>
2026-03-31 12:17:01 -0500

    History for revision 125.

--- code/smallc22/notice.h
+++ code/smallc22/notice.h
@@ -1,3 +1,3 @@
 // NOTICE.H -- Small C Signon Notice.
-#define VERSION "Small C, Version 2.3, Revision Level 124\n"
+#define VERSION "Small C, Version 2.3, Revision Level 125\n"
 #define CRIGHT1 "Copyright 1982, 1983, 1985, 1988 J. E. Hendrix\n\n"
